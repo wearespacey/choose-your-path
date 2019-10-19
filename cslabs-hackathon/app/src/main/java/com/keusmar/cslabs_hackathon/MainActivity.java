2c7e59f911af912f9b0ce0765c53559b32ff673d
Guillaume <gservais98@gmail.com>
2019-10-19 10:30:46 +0200

    Use data

--- cslabs-hackathon/app/src/main/java/com/keusmar/cslabs_hackathon/MainActivity.java
+++ cslabs-hackathon/app/src/main/java/com/keusmar/cslabs_hackathon/MainActivity.java
@@ -3,10 +3,15 @@ package com.keusmar.cslabs_hackathon;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.SurfaceView;
 
 import com.keusmar.cslabs_hackathon.Data.GameImpactData;
 import com.keusmar.cslabs_hackathon.Data.ReadData;
+import com.keusmar.cslabs_hackathon.Models.Action;
+import com.keusmar.cslabs_hackathon.Models.CaracteristicConstants;
+import com.keusmar.cslabs_hackathon.Models.GameImpact;
+import com.keusmar.cslabs_hackathon.Models.Impact;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +19,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(new MainView(this));
+        ReadData rd = new ReadData(this);
+        ArrayList<Action> actions = rd.getActions();
         GameImpactData gid = new GameImpactData();
+
+        for(Action action:actions){
+            for(Impact impact:action.getImpacts()){
+                gid.applyGameImpact(impact, true);
+            }
+        }
+
+        ArrayList<GameImpact> impacts = gid.getGameImpacts();
+        GameImpact earth = gid.getGameImpactByName(CaracteristicConstants.EARTH);
+        GameImpact test = gid.getGameImpactByName("Yeet");
+
     }
 }
--- cslabs-hackathon/app/src/main/java/com/keusmar/cslabs_hackathon/Models/Impact.java
+++ cslabs-hackathon/app/src/main/java/com/keusmar/cslabs_hackathon/Models/Impact.java
@@ -3,6 +3,8 @@ package com.keusmar.cslabs_hackathon.Models;
 public class Impact {
     private Float points;
     private String caracteristic;
+    private Float pointsYes;
+    private Float pointsNo;
 
     public Impact(){}
 
@@ -25,5 +27,21 @@ public class Impact {
 
     public void setCaracteristic(String caracteristic) {
         this.caracteristic = caracteristic;
+    }
+
+    public Float getPointsYes() {
+        return pointsYes;
+    }
+
+    public void setPointsYes(Float pointsYes) {
+        this.pointsYes = pointsYes;
+    }
+
+    public Float getPointsNo() {
+        return pointsNo;
+    }
+
+    public void setPointsNo(Float pointsNo) {
+        this.pointsNo = pointsNo;
     }
 }
