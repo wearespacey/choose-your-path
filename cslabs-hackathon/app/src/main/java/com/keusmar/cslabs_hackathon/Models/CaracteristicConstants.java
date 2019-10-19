80ac7a4d949a18b2ee3d1aaad5780ab0a36d4bac
Guillaume <gservais98@gmail.com>
2019-10-19 15:12:13 +0200

    impacts reworked

--- cslabs-hackathon/app/src/main/java/com/keusmar/cslabs_hackathon/Models/CaracteristicConstants.java
+++ cslabs-hackathon/app/src/main/java/com/keusmar/cslabs_hackathon/Models/CaracteristicConstants.java
@@ -3,16 +3,14 @@ package com.keusmar.cslabs_hackathon.Models;
 import java.util.ArrayList;
 
 public final class CaracteristicConstants {
-    public static final String MONEY = "Money";
-    public static final String HEALTH = "Health";
-    public static final String FRIENDSHIP = "Friendship";
-    public static final String EARTH = "Earth";
+    public static final String ECONOMY = "Economy";
+    public static final String ECOLOGY = "Ecology";
+    public static final String COMFORT = "Comfort";
     public static final ArrayList<String> ALL_CARACTERISITCS = new ArrayList<String>(){
         {
-            add(MONEY);
-            add(HEALTH);
-            add(FRIENDSHIP);
-            add(EARTH);
+            add(ECONOMY);
+            add(ECOLOGY);
+            add(COMFORT);
         }
     };
 }
